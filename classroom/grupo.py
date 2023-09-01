d9bffb1365df9938a0441b0bad5b3f82bd4cbb16
santiagogiraldo128 <santiagogiraldo128@gmail.com>
2023-08-31 22:29:22 -0500

    Tercer prueba

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,7 +3,7 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
@@ -23,7 +23,7 @@ class Grupo:
         self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
-        cadena = "Grupo de estudiantes:" + self._grupo
+        cadena = "Grupo de estudiantes: " + self._grupo
         return cadena
     
 
